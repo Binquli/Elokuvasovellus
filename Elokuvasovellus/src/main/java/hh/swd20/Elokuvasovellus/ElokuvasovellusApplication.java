d8287daf227924d7f60bcc341a07cf0d785d9ccf
Joonas <bgg306@myy.haaga-helia.fi>
2019-11-07 11:54:31 +0200

    first commit

---
+++ Elokuvasovellus/src/main/java/hh/swd20/Elokuvasovellus/ElokuvasovellusApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.Elokuvasovellus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElokuvasovellusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElokuvasovellusApplication.class, args);
+	}
+
+}
---
+++ Elokuvasovellus/src/test/java/hh/swd20/Elokuvasovellus/ElokuvasovellusApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd20.Elokuvasovellus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElokuvasovellusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
